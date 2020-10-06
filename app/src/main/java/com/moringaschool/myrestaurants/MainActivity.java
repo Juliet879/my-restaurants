be8308f3cd669cef44fb1a034f163f580bbd3fd3
JulietK <julietgisemba000@>
2020-10-06 20:56:12 +0300

    add test

--- app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -2,13 +2,36 @@ package com.moringaschool.myrestaurants;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button mFindRestaurantButton;
+    private EditText mlocationEditText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mlocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mFindRestaurantButton = (Button)findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantButton.setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View v) {
+                      String location = mlocationEditText.getText().toString();
+                      Log.d(TAG, location);
+                      Intent intent = new Intent(MainActivity.this,RestaurantsActivity.class);
+                      intent.putExtra("location",location);
+                      startActivity(intent);
+                  }
+              });
     }
 }
